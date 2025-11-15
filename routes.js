991fd479685dcfe94fa36936424c734144b324b2
W0198745JC <w0198745@campus.nscc.ca>
2025-11-15 14:50:54 -0400

    connected Docker Database

--- routes.js
+++ routes.js
@@ -5,18 +5,23 @@ const router = express.Router();
 
 //Routes (API Endpoints)
 //GET/api/shows/
-router.get('/', (req, res) => {
-    //Create some sample show data 
-   await sql.connect(/*import db server etc here*/)
+router.get('/', async (req, res) => {
+   
+   await sql.connect('Server=localhost,1433; Database=master; User Id=sa; Password=57mJok51[fgN;TrustServerCertificate=True')
+   
+   const result = await sql.query `select * from mytable where id = ${value}`
+   console.dir(result.recordset) //return the result as json
 
-   const result = awit sql.query `select`
   res.json(shows);
-});
 
+});
+//GEt: /api/shows/1
 router.get('/:id', (req, res) => {
     const id = req.params.id;
-    res.send('Show Details for Show ID: ${id}');
+    res.send(`Show Details for Show ID: ${id}`);
 
     const show = {id: id, title: 'sample show', description: 'This is a sample description'}
-})
+
+    res.json(show)
+});
 export default router;
